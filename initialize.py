ce1bcfcac3dd6765904894699e38dbf7d2d7df3e
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-03-20 13:21:12 +0530

    Adds code to create database if doesn't exists already

--- initialize.py
+++ initialize.py
@@ -1,10 +1,35 @@
 import re
 import sys
+import psycopg2 as Database
 
 from django.conf import settings
 from django.db import connection
+from django.db.utils import OperationalError
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+def create_database(connection_params):
+  params = connection_params.copy()
+  database = params.pop('database')
+  params['database'] = 'postgres'
+  conn = Database.connect(**params)
+  conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+  cursor = conn.cursor()
+  cursor.execute('CREATE DATABASE ' + database)
+  cursor.close()
+  conn.close()
+
 
 if __name__ == '__main__':
+  params = connection.get_connection_params()
+  try:
+    connection.ensure_connection()
+  except OperationalError as error:
+    if 'database "' + params['database'] + '" does not exist' in error.message:
+      create_database(params)
+    else:
+      raise error
+
   # skipping if already some tables exists
   if connection.introspection.table_names():
     sys.exit(0)

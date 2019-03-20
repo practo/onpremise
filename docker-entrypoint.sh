7decd5b7f4e528947c822a7785bb03638a9dfc51
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2019-03-20 10:55:28 +0530

    Fixes if else conditions in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,16 @@
 set +e
 
 if [[ -z ${run_migration} ]]; then
+  if [[ -z ${worker_cmd} ]]; then
+    sentry run web
+  else
+    eval ${worker_cmd}
+  fi
+else
   # Loads initial schema if doesn't already exists
   sentry exec initialize.py
   # Runs sentry migrations
   sentry upgrade
   # Load fixtures
   sentry exec sync_fixtures.py
-elif [[ -z ${worker_cmd} ]]; then
-  sentry run web
-else
-  eval ${worker_cmd}
 fi
